0f8dea8387da88e12c1bfaa19033337a258c780b
Khairul Ikhsan Nurzeha <32305997+ikhsannz@users.noreply.github.com>
2017-11-08 14:55:56 +0800

    membuat inputan

--- nomor3.cpp
+++ nomor3.cpp
@@ -4,4 +4,14 @@ using namespace std;
 
 int main(){
 	double jk,gp,gt,gb,jt,jkr,j(1.5);
+
+	
+	cout<<"masukkan total jam kerja\t:";
+	cin>>jk;
+	j=jk-40;
+	jkr=j*1.5;
+	
+	cout<<"masukkan gaji per jam\t\t:";
+	cin>>gp;
+	
 }
