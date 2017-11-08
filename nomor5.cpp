47d3dcdb28fe9d685df5dc6ea791452e6c8ac10f
Khairul Ikhsan Nurzeha <32305997+ikhsannz@users.noreply.github.com>
2017-11-08 15:19:52 +0800

    merapikan dan menambahkan variasi ya dan tidak

--- nomor5.cpp
+++ nomor5.cpp
@@ -4,20 +4,34 @@ using namespace std;
 
 int main(){
 	double berat, tinggi  (2.5);
+	char yt;
 	
+	awal:
+	cout<<"====================================\n";
 	cout<<"masukkan tinggi badan(cm)\t:";
 	cin>>tinggi;
 	cout<<"masukkan berat badan(kg)\t:";
 	cin>>berat;
+	cout<<"====================================\n";
 
 	if (berat<tinggi/2.5)
-	cout<<"anda terlalu kurus = underweight";
+	cout<<"anda terlalu kurus = underweight\n";
 		
 	else if (tinggi/2.3<berat)
-	cout<<"anda terlalu berat = overweight";	
+	cout<<"anda terlalu berat = overweight\n";	
 		
 	else if (tinggi/2.5<=berat<=tinggi/2.3)
-	cout<<"berat dan tinggi badan anda normal";
+	cout<<"berat dan tinggi badan anda normal\n";
 	else
 	cout<<"program error, silahkan coba lagi";
+	
+	cout<<"====================================\n";
+	cout<<"Apakah anda ingin mengulang ? [Y/T] : ";
+	cin>>yt;
+	if(yt=='Y' || yt=='y')
+	{goto awal;}
+	if(yt=='T' || yt=='t')
+	{goto selesai;}
+	selesai:
+	cout<<"Terima Kasih";
 }
