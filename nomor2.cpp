8d5579b070419cf2727481e6dfbb8d578a4f5187
Khairul Ikhsan Nurzeha <32305997+ikhsannz@users.noreply.github.com>
2017-11-08 14:47:41 +0800

    membuat inputan dan informasi yang akan di input

--- nomor2.cpp
+++ nomor2.cpp
@@ -5,4 +5,16 @@ using namespace std;
 int main(){
 	char q;
 	double Ls,Lpp,p,l,a,t(2.5);
+
+	cout <<"=====================================\n";
+	cout <<"PROGRAM PERHITUNGAN LUAS BANGUN DATAR\n";
+	cout <<"=====================================\n";
+	cout <<"  +KODE+   LUAS BANGUN DATAR   +\n";
+	cout <<"  +  P +  Luas persegi panjang +\n";
+	cout <<"  +  S +    Luas segitiga      +\n";
+	cout <<"=====================================\n";
+	cout <<"silahkan masukkan kode yang dipilih:";
+	cin >>q;
+
+
 }
