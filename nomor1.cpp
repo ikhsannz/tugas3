80b875c3180f7d5eaa670c3740982ed6c06dbcf6
Khairul Ikhsan Nurzeha <32305997+ikhsannz@users.noreply.github.com>
2017-11-08 14:37:43 +0800

    menambahkan variasi ya dan tidak

--- nomor1.cpp
+++ nomor1.cpp
@@ -6,6 +6,7 @@ int main(){
 	int x,y,hsl;
 	char yt;
 	
+	awal:
 	cout<<"program pengurangan tanpa negatif\n";
 	cout<<"masukkan x\t:";
 	cin>>x;
@@ -18,4 +19,14 @@ int main(){
 	hsl=x-y;
 	
 	cout<<"hasil\t\t:"<<hsl<<"\n";
+	
+	cout<<"Apakah anda ingin mengulang ? [Y/T] : ";
+	cin>>yt;
+	if(yt=='Y' || yt=='y')
+	{goto awal;}
+	if(yt=='T' || yt=='t')
+	{goto selesai;}
+	
+	selesai:
+	cout<<"Terima Kasih";
 }
