4d85332c74adf7797d7513ad2e1ff29976fc8e68
Khairul Ikhsan Nurzeha <32305997+ikhsannz@users.noreply.github.com>
2017-11-08 14:58:43 +0800

    menambah variasi ya dan tidak

--- nomor3.cpp
+++ nomor3.cpp
@@ -4,8 +4,9 @@ using namespace std;
 
 int main(){
 	double jk,gp,gt,gb,jt,jkr,j(1.5);
-
+	char yt;
 	
+	awal:
 	cout<<"masukkan total jam kerja\t:";
 	cin>>jk;
 	j=jk-40;
@@ -21,4 +22,13 @@ int main(){
 	cout<<"total jam kerja\t\t\t:"<<jt<<"\n";
 	cout<<"total gaji \t\t\t:"<<gt<<"\n";
 	cout<<"gaji bersih \t\t\t:"<<gb<<"\n";
+	
+	cout<<"Apakah anda ingin mengulang ? [Y/T] : ";
+	cin>>yt;
+	if(yt=='Y' || yt=='y')
+	{goto awal;}
+	if(yt=='T' || yt=='t')
+	{goto selesai;}
+	selesai:
+	cout<<"Terima Kasih";
 }
