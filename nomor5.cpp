aca7381367337c762bc049346992223f93c0dfb4
Khairul Ikhsan Nurzeha <32305997+ikhsannz@users.noreply.github.com>
2017-11-08 15:16:52 +0800

    membuat rumus dan outputan dengan if

--- nomor5.cpp
+++ nomor5.cpp
@@ -9,4 +9,15 @@ int main(){
 	cin>>tinggi;
 	cout<<"masukkan berat badan(kg)\t:";
 	cin>>berat;
+
+	if (berat<tinggi/2.5)
+	cout<<"anda terlalu kurus = underweight";
+		
+	else if (tinggi/2.3<berat)
+	cout<<"anda terlalu berat = overweight";	
+		
+	else if (tinggi/2.5<=berat<=tinggi/2.3)
+	cout<<"berat dan tinggi badan anda normal";
+	else
+	cout<<"program error, silahkan coba lagi";
 }
