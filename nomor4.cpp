20ee142e1bfb9f00e69333eeafc1cfca75eac9db
Khairul Ikhsan Nurzeha <32305997+ikhsannz@users.noreply.github.com>
2017-11-08 15:03:24 +0800

    membuat inputan

--- nomor4.cpp
+++ nomor4.cpp
@@ -4,4 +4,13 @@ using namespace std;
 
 int main(){
 	int x;
+	
+	cout<<"============================\n";
+	cout<<"PROGRAM MENENTUKAN BILANGAN\n";
+	cout<<"POSTIF ATAU NEGATIF ATAU NOL\n";
+	cout<<"============================\n";
+	
+	cout<<"masukkan nilai atau angka:";
+	cin>>x;
+	cout<<"============================\n";
 }
